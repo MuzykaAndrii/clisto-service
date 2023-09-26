4c3cdeef20ad8cd69de8e80a4ae75e3fe94ee8c1
MuzykaAndrii <andrymyzik@gmail.com>
2023-09-26 22:39:47 +0300

    added config module

---
+++ backend/app/config.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from typing import Literal
+
+from pydantic import computed_field
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_file_path = BASE_DIR / ".env"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=env_file_path, env_file_encoding="utf-8")
+
+    DEBUG: bool
+    MODE: Literal["DEV", "TEST", "PROD"]
+
+    ORIGINS: list[str]
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
+    DB_URL_PATTERN: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_NAME: str
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_URL_PATTERN: str
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    PGADMIN_DEFAULT_EMAIL: str
+    PGADMIN_DEFAULT_PASSWORD: str
+
+    AUTH_TOKEN_NAME: str
+    JWT_SECRET: str
+    JWT_EXPIRE_MINUTES: int
+
+    @computed_field
+    @property
+    def database_url(self) -> str:
+        return self.DB_URL_PATTERN.format(
+            user=self.DB_USER,
+            password=self.DB_PASS,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            name=self.DB_NAME,
+        )
+
+    @computed_field
+    @property
+    def test_database_url(self) -> str:
+        return self.TEST_DB_URL_PATTERN.format(
+            user=self.TEST_DB_USER,
+            password=self.TEST_DB_PASS,
+            host=self.TEST_DB_HOST,
+            port=self.TEST_DB_PORT,
+            name=self.TEST_DB_NAME,
+        )
+
+
+settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import settings
 
 
 app = FastAPI(
     title="Clisto service",
+    debug=settings.DEBUG,
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
+
 
 @app.get("/ping/")
 async def pong():
