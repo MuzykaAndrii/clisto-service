1561195b88ad01a810150ccc5a8901c1568ec8e8
MuzykaAndrii <andrymyzik@gmail.com>
2023-09-27 16:39:06 +0300

    changed backend entry point

---
+++ backend/scripts/backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alembic upgrade head
+
+if [[ "${1}" == "prod" ]]; then
+    gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --reload
+elif [[ "${1}" == "dev" ]]; then
+    uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+fi
