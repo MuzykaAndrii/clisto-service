4688da70d8f39618f4fa4874dc61f0e19fa3ab48
MuzykaAndrii <andrymyzik@gmail.com>
2023-09-27 15:13:09 +0300

    implemented user model

---
+++ backend/app/modules/user/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(length=50), unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+    is_superuser = Column(Boolean, default=False, nullable=False)
