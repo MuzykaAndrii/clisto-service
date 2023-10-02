161b502a30b78738e4e9491df2c476fa5f29cbf7
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-03 00:05:13 +0300

    implemented base user exceptions

--- backend/app/modules/users/exceptions.py
+++ backend/app/modules/users/exceptions.py
@@ -1,3 +1,19 @@
+class UserError(Exception):
+    pass
+
+
+class UserLoginError(UserError):
+    pass
+
+
+class UserNotFoundError(UserLoginError):
+    pass
+
+
+class UserInvalidPassword(UserLoginError):
+    pass
+
+
 class JwtNotValidError(Exception):
     pass
 
