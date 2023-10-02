abfb6c5e30ce95a8a360828944ea5ad42f38db54
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-02 18:39:30 +0300

    defined main router in pages

--- backend/app/modules/pages/routes.py
+++ backend/app/modules/pages/routes.py
@@ -1,10 +1,12 @@
 from fastapi import (
     APIRouter,
+    Depends,
     Request,
 )
 from fastapi.templating import Jinja2Templates
 
 from app.config import BASE_DIR
+from app.modules.auto_maintenance.routes import get_categories
 
 router = APIRouter(
     prefix="/pages",
@@ -15,8 +17,14 @@ templates = Jinja2Templates(directory=BASE_DIR / "app/templates")
 
 
 @router.get("/main")
-async def get_main_page(request: Request):
+async def get_main_page(
+    request: Request,
+    categories=Depends(get_categories),
+):
     return templates.TemplateResponse(
         name="main.html",
-        context={"request": request},
+        context={
+            "request": request,
+            "categories": categories,
+        },
     )
