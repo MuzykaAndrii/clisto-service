756f8747db5d4fb426d5074cc292b8b58b756e8a
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-03 00:05:46 +0300

    implemented authenticate user service

---
+++ backend/app/modules/users/services/auth.py
@@ -0,0 +1,24 @@
+from app.modules.users.dal import UserDAL
+from app.modules.users.exceptions import (
+    UserInvalidPassword,
+    UserNotFoundError,
+)
+from app.modules.users.models import User
+from app.modules.users.schemas import UserLogin
+from app.modules.users.services.password import PWDService
+
+
+async def authenticate_user(user_in: UserLogin) -> User:
+    user = await UserDAL.get_user_by_email_or_username(user_in.username_or_email)
+
+    if not user:
+        raise UserNotFoundError
+
+    pass_matching = PWDService.verify_password(
+        raw_password=user_in.password, hashed_password=user.password_hash
+    )
+
+    if not pass_matching:
+        raise UserInvalidPassword
+
+    return user
