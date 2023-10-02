ac018bb8bb17a82983b443c35e5877b165c647b6
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-03 00:12:26 +0300

    refactored auth service

--- backend/app/modules/users/services/auth.py
+++ backend/app/modules/users/services/auth.py
@@ -8,17 +8,19 @@ from app.modules.users.schemas import UserLogin
 from app.modules.users.services.password import PWDService
 
 
-async def authenticate_user(user_in: UserLogin) -> User:
-    user = await UserDAL.get_user_by_email_or_username(user_in.username_or_email)
+class AuthService:
+    @staticmethod
+    async def authenticate_user(user_in: UserLogin) -> User:
+        user = await UserDAL.get_user_by_email_or_username(user_in.username_or_email)
 
-    if not user:
-        raise UserNotFoundError
+        if not user:
+            raise UserNotFoundError
 
-    pass_matching = PWDService.verify_password(
-        raw_password=user_in.password, hashed_password=user.password_hash
-    )
+        pass_matching = PWDService.verify_password(
+            raw_password=user_in.password, hashed_password=user.password_hash
+        )
 
-    if not pass_matching:
-        raise UserInvalidPassword
+        if not pass_matching:
+            raise UserInvalidPassword
 
-    return user
+        return user
