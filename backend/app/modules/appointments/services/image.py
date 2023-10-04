2a8f1cb1807107569e9ed263e883f9cc8b2f4c2f
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-05 00:12:15 +0300

    implemented image service

---
+++ backend/app/modules/appointments/services/image.py
@@ -0,0 +1,9 @@
+from app.files.services import (
+    FileService,
+    MimeTypes,
+)
+
+
+class ImageService(FileService):
+    max_file_size: int = 5 * 1024 * 1024
+    expected_file_type: MimeTypes = MimeTypes.image
