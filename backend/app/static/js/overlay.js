fd42b9cd4ec8f29f18f9d48dd32dd4c404ebfcde
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-12 23:16:23 +0300

    implemented pausing video after closing overlay

--- backend/app/static/js/overlay.js
+++ backend/app/static/js/overlay.js
@@ -1,6 +1,8 @@
 const overlayContainer = document.querySelector('.overlay-container');
 const closeBtn = document.querySelector('.overlay-close-btn');
+const overlayVideo = document.querySelector('.overlay-video iframe');
 
 closeBtn.addEventListener('click', function () {
     overlayContainer.style.display = 'none';
+    overlayVideo.src = "";
 });
