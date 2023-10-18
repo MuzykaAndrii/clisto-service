2da543c96c84144552b1d963160d84df54a613f3
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-18 18:19:26 +0300

    fixed return types in mime type helper

--- backend/app/emails/utils.py
+++ backend/app/emails/utils.py
@@ -3,12 +3,14 @@ from fastapi import UploadFile
 
 class MimeTypeHelper:
     def __init__(self, file: UploadFile):
+        self._mime_type: str
+        self._mime_subtype: str
         self._mime_type, self._mime_subtype = file.content_type.split("/")
 
     @property
-    def mime_type(self):
+    def mime_type(self) -> str:
         return self._mime_type
 
     @property
-    def mime_subtype(self):
+    def mime_subtype(self) -> str:
         return self._mime_subtype
