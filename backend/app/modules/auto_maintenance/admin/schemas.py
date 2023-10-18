e34bce335e7eb843f2137e0cc3b9a40553d75461
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-18 19:11:40 +0300

    fixed types in admin schemas in auto maintenance app

--- backend/app/modules/auto_maintenance/admin/schemas.py
+++ backend/app/modules/auto_maintenance/admin/schemas.py
@@ -11,13 +11,13 @@ from pydantic import (
 class ServiceOptionAdminSchema(BaseModel):
     name: str = Field(max_length=50, min_length=4)
     description: str | None = None
-    video_url: AnyHttpUrl | None = None
+    video_url: AnyHttpUrl | str = ""
     subcategory: Any
     icon: Any
 
     @field_validator("video_url", mode="before")
     @classmethod
-    def video_url_empty_to_none(cls, value):
+    def video_url_empty_to_none(cls, value) -> str | None:  # type: ignore
         if value == "":
             return None
         return value
@@ -25,10 +25,10 @@ class ServiceOptionAdminSchema(BaseModel):
 
 class SubCategoryAdminSchema(BaseModel):
     name: str = Field(max_length=50, min_length=4)
-    service_options: list[Any] = None
+    service_options: list[Any] | None = None
     category: Any
 
 
 class CategoryAdminSchema(BaseModel):
     name: str = Field(max_length=50, min_length=4)
-    subcategories: list[Any] = None
+    subcategories: list[Any] | None = None
