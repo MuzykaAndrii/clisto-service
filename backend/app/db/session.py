713c8b71903705d47a100a7f36a303d8c888ad09
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-18 19:56:33 +0300

    fixed types in db session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -15,7 +15,7 @@ else:
     engine = create_async_engine(settings.database_url)
 
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
