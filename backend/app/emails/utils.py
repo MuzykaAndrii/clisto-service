7365b7f1baa48953723fb3e867f040a471a845e3
MuzykaAndrii <andrymyzik@gmail.com>
2023-10-25 16:48:25 +0300

    refactored mime type helper

--- backend/app/emails/utils.py
+++ backend/app/emails/utils.py
@@ -1,10 +1,18 @@
 from fastapi import UploadFile
 
 
+class InvalidFileTypeError(Exception):
+    pass
+
+
 class MimeTypeHelper:
     def __init__(self, file: UploadFile):
         self._mime_type: str
         self._mime_subtype: str
+
+        if not file.content_type:
+            raise InvalidFileTypeError
+
         self._mime_type, self._mime_subtype = file.content_type.split("/")
 
     @property
